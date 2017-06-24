ef6292638a57f3deabbf52f8989da28772db0a76
eaytac8 <elif.aytc@hotmail.com>
2017-06-25 02:06:30 +0300

    run metodu

--- src/asalsayithread/AsalSayiThread.java
+++ src/asalsayithread/AsalSayiThread.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package asalsayithread;
 
-/**
- *
- * @author ASUS
- */
-public class AsalSayiThread {
+public class AsalSayiThread extends Thread
+{
+    int ilk, son;
+    int sayidizisi[];
+    
+    public AsalSayiThread(int sayidizisi[], int ilk, int son)
+    {
+        this.sayidizisi = sayidizisi;
+        this.ilk = ilk;
+        this.son = son;
+    }
+    
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        int sayidizisi[] = new int[10000];
+        
+        for(int i=0; i<sayidizisi.length; i++)
+        {
+            int random = (int)(Math.random()*100);
+            sayidizisi[i] = random;
+        }
+        
+        AsalSayiThread m1 = new AsalSayiThread(sayidizisi,0,2500);
+        AsalSayiThread m2 = new AsalSayiThread(sayidizisi,2500,5000);
+        AsalSayiThread m3 = new AsalSayiThread(sayidizisi,5000,7500);
+        AsalSayiThread m4 = new AsalSayiThread(sayidizisi,7500,10000);
+        
+        m1.start();
+        m2.start();
+        m3.start();
+        m4.start();
+        
     }
     
 }
